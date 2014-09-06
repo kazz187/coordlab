ed859a3bbf6513378a71894a0dce5cc9e05608bc
gong023 <gon.gong.gone@gmail.com>
2014-09-06 14:54:39 +0900

    add search dropdown

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class ChatController < ApplicationController
   include ActionController::Live
+  before_filter :set_default_val
 
   @@streams ||= []
 
@@ -35,5 +36,27 @@ class ChatController < ApplicationController
     end
 
     render :index
+  end
+
+  def set_default_val
+    @category_id1 = category_id1
+  end
+
+  def category_id1
+    {
+      10 => 'アウター',
+      11 => 'トップス',
+      12 => 'ボトムス',
+      13 => 'シューズ',
+      14 => 'バッグ',
+      15 => 'アクセ',
+      16 => '帽子',
+      17 => '装飾小物',
+      18 => 'ルームウェア',
+      19 => 'コスメ',
+      20 => '雑貨',
+      21 => '水着・浴衣',
+      22 => 'デコ素材'
+    }
   end
 end
