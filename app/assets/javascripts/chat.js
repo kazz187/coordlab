1fc380788e535a3f9bb88a6e5765d7f94395e466
gong023 <gon.gong.gone@gmail.com>
2014-09-06 16:44:05 +0900

    fix icon bug

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,5 +1,5 @@
 var chat = function(attr) {
-  var icon = '<img class="icon" width="50" height="50" src="' + attr.icon + '"/>';
+  var icon = '<img class="icon" width="50" height="50" src="' + decodeURIComponent(attr.icon) + '"/>';
   var name = '<div class="name">' + attr.name + '</div>';
   var message = '<div class="message">' + attr.comment + '</div>';
   $("#chat_area").append('<li>' + icon + name + message + '</li>');
@@ -24,3 +24,10 @@ sse.onmessage = function(event) {
         break;
     }
 };
+
+$(function() {
+  $("#message_form_submit").on('click', function() {
+    var icon_url = encodeURIComponent('https://pbs.twimg.com/profile_images/592317390/twitter_400x400.png');
+    $('div.message-form > form').append('<input name="icon" type="hidden" value="' + icon_url + '">');
+  });
+});
